19b9165dc6f0d3c05fdb76a115c986e7c09c42dd
sainathdora <dheerajkamepalli1412@gmail.com>
2025-03-02 18:00:18 +0530

    updated header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -12,6 +12,7 @@ const Header = () => {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/Register">Register</Link></li>
             <li><Link to="/login">Login</Link></li>
+            <li><Link to="/Dashboard">Dashboard</Link></li>
             <li><Link to="/profile">Profile</Link></li>
           </ul>
         </nav>
