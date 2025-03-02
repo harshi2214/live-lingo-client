e7f64b1b270dfae028fb6e91a63f82ef4d8a267e
sainathdora <dheerajkamepalli1412@gmail.com>
2025-03-02 17:05:40 +0530

    Updated Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -10,10 +10,9 @@ const Header = () => {
         <nav>
           <ul className="nav-links">
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/chat">Chat</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/login">Login</Link></li>
             <li><Link to="/Register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
           </ul>
         </nav>
       </div>
